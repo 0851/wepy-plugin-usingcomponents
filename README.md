2c42d86f9c3f0a2f5a9b264e74dce9baec845364
wangjianliang <wangjianliang@kingsoft.com>
2018-08-05 20:22:14 +0800

    change

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # wepy-plugin-usingcomponents
 wepy-plugin-usingcomponents auto require wx min app usingcomponents
+
+自动装载usingcomponents
+
+## 安装
+
+```
+npm install wepy-plugin-usingcomponents --save-dev
+```
+
+## 配置`wepy.config.js`
+
+```
+const path = require("path");
+module.exports.plugins = {
+    'usingcomponents': {
+        dirs = [path.resolve(__dirname, 'node_modules')],
+        filter = /\.json$/,
+        dist = "dist/usingcomponents"
+    }
+};
+```
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
+import copydir from 'copy-dir';
+import mkdirp from 'mkdirp';
 
+function statSync(f) {
+    try {
+        return fs.statSync(f)
+    } catch (error) {
+        console.error(error);
+        return null
+    }
+}
 export default class {
-    constructor({
-        dirs = [],
-        filter = "*.config"
-    }) {
+    constructor(config) {
+        this.setting = Object.assign({
+            dirs: [path.resolve(process.cwd(), 'node_modules')],
+            filter: /\.json$/,
+            dist: "dist"
+        }, config);
+
+        let dirs = this.setting.dirs;
 
         if (!_.isArray(dirs)) {
             dirs = [dirs]
         }
-
+        this.setting.dirs = dirs;
 
         const noDir = _.find(dirs, (dir) => {
-            const stat = fs.statSync(dir);
-            return !stat.isDirectory()
+            const stat = statSync(dir);
+            return stat && !stat.isDirectory()
         })
 
         if (noDir) {
@@ -23,43 +37,63 @@ export default class {
             process.exit(1);
             return;
         }
-
-        this.setting = {
-            dirs,
-            filter
-        };
     }
     apply(op) {
-
         let setting = this.setting;
+        if (!op.code) {
+            op.next();
+            return
+        }
+        if (!setting.filter.test(op.file)) {
+            op.next();
+            return
+        }
+        const config = JSON.parse(op.code);
 
-        if (op.code !== null && setting.filter.test(op.file)) {
-            op.output && op.output({
-                action: '变更',
-                file: op.file
-            });
-
-            let config = setting.config;
-            let configs = [];
-
-            if (config instanceof Array) {
-                configs = configs.concat(config);
-            } else if (config instanceof Object && !config.find) {
-                for (let key in config) {
-                    let value = config[key];
-                    if (value.find) {
-                        configs.push(value);
-                    }
-                }
-            } else if (config instanceof Object && config.find) {
-                configs.push(config);
-            }
-
-            configs.forEach((config) => {
-                op.code = op.code.replace(config.find, config.replace);
-            })
+        if (!config.usingComponents) {
+            op.next();
+            return
         }
 
+        const dirs = setting.dirs;
+        const files = []
+        _.each(dirs, (dir) => {
+            _.each(Object.keys(config.usingComponents), (key) => {
+                const s = config.usingComponents[key];
+                const p = path.resolve(dir, s);
+                files.push({
+                    p: p,
+                    s: s,
+                    k: key
+                });
+            })
+        })
+
+        if (!files.length) {
+            op.next();
+            return
+        }
+
+        op.output && op.output({
+            action: '编译',
+            file: op.file
+        });
+
+        _.each(files, (file) => {
+            const stat = statSync(file.p);
+            if (stat && stat.isDirectory()) {
+                const usingcomponents = `usingcomponents/${file.s}`
+                const dist = `${setting.dist}/${usingcomponents}`
+                const p = path.resolve(process.cwd(), dist);
+                mkdirp.sync(p);
+                copydir.sync(file.p, p);
+                const f = path.parse(op.file);
+                const relative = path.relative(f.dir, p);
+                config.usingComponents[file.k] = `${relative}`;
+            }
+        })
+
+        op.code = JSON.stringify(config);
 
         op.next();
     }
